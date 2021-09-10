1284cd298d554f62a410dc8f50503c68e7424656
PracticasCoeur <practicascoeur@gmail.com>
2021-09-10 13:01:16 -0500

    Antes-Despues lo acomode diferente

--- src/paths/antes-despues.path.php
+++ src/paths/antes-despues.path.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="texto">
             <h1 class="titulo blanco">Antes y Despues</h1>
-            <center><button class="boton_cita">Agenda tu cita</button></center>
+            <button class="boton_cita">Agenda tu cita</button>
         </div>
     </div>
 </section>
@@ -17,12 +17,12 @@
 <input type="button" value="Caso 4">
 <input type="button" value="Caso 5">
 </div>
-
+</center>
 <div id="comparison">
   <figure>
     <div id="divisor"></div>
   </figure>
   <input type="range" min="0" max="100" value="50" id="slider" oninput="moveDivisor()">
 </div>
-</center>
+
 </section>
