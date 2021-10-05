354b6a95880dcca3c4942c42ac34789373ebc817
Santiago M - CoeurMkt <desarrollo@coeurmkt.com>
2021-10-05 11:25:49 -0500

    Arreglos esteticos

--- src/paths/index.path.php
+++ src/paths/index.path.php
@@ -349,40 +349,48 @@
 <!-- Contactame -->
 <section id="contactame">
     <h1 class="titulo">contáctame</h1>
-    <div class="container">
-        <div class="columns is-desktop">
-            <div class="column">
-                <h2>Donde encontrarme</h2>
-                <div class="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11764.135006578505!2d-105.29817592435406!3d20.697166735683428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x58d0d5a503433441!2sHospital%20Joya%20Riveria%20Nayarit!5e0!3m2!1sen!2sar!4v1624557421088!5m2!1sen!2sar" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                </div>
-            </div>
-            <div class="column">
-                <h2>Agenda tu cita</h2>
-                <div class="columns is-mobile is-multiline is-1">
-                    <div class="column is-one-quarter">
-                        <h4 class="whatsapp">Whatsapp</h4>
-                    </div>
-                    <div class="column is-three-quarters">
-                        <p>(322) 291 2787</p>
-                    </div>
-                    <div class="column is-one-quarter">
-                        <h4 class="horario">Horario</h4>
-                    </div>
-                    <div class="column is-three-quarters">
-                        <p>Lunes - Viernes de 10 am a 7 pm</p>
-                    </div>
-                    <div class="column is-one-quarter">
-                        <h4 class="ubicacion">Ubicación</h4>
-                    </div>
-                    <div class="column is-three-quarters">
-                        <p>Hospital Joya Nuevo Vallarta</p>
-                    </div>
-                </div>
+    <div class="columns is-desktop">
+        <div class="column">
+            <div class="contenedor_boton">
+                <button class="boton_cita">Agenda tu cita</button>
             </div>
         </div>
-        <div class="contenedor_boton">
-            <button class="boton_cita">Agenda tu cita</button>
+    </div>
+    <div class="columns is-desktop">
+        <div class="column">
+            <h2>Donde encontrarme</h2>
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11764.135006578505!2d-105.29817592435406!3d20.697166735683428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x58d0d5a503433441!2sHospital%20Joya%20Riveria%20Nayarit!5e0!3m2!1sen!2sar!4v1624557421088!5m2!1sen!2sar" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
+        <div class="column">
+            <h2>Agenda tu cita</h2>
+            <div class="columns is-mobile is-multiline is-1">
+                <div class="column is-one-quarter">
+                    <h4 class="whatsapp">Whatsapp</h4>
+                </div>
+                <div class="column is-three-quarters">
+                    <p>(322) 291 2787</p>
+                </div>
+                <div class="column is-one-quarter">
+                    <h4 class="whatsapp">Telefono</h4>
+                </div>
+                <div class="column is-three-quarters">
+                    <p>(346) 7856 7876</p>
+                </div>
+                <div class="column is-one-quarter">
+                    <h4 class="horario">Horario</h4>
+                </div>
+                <div class="column is-three-quarters">
+                    <p>Lunes - Viernes de 10 am a 7 pm</p>
+                </div>
+                <div class="column is-one-quarter">
+                    <h4 class="ubicacion">Ubicación</h4>
+                </div>
+                <div class="column is-three-quarters">
+                    <p>Hospital Joya Nuevo Vallarta</p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
