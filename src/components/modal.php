875750190be7910a9f08b7e7946f2d9a1742cf4c
Santiago M - CoeurMkt <desarrollo@coeurmkt.com>
2021-10-05 09:58:17 -0500

    Cambios a modal, para que sean 2 columnas con iconos de 1 rem (16px) | en movil se desaparece el icono

--- src/components/modal.php
+++ src/components/modal.php
@@ -1,15 +1,19 @@
 <div class="modal" id="modal">
   <div class="modal-background"></div>
   <div class="modal-content">
-      
-        <div class="column">
-          <center>
-            <p>WhatsApp</p><br><a href="https://wa.me/523222912787"><img src="../assets/img/whatsapp.png" alt="" srcset=""></a><br><br>
-            <p>Teléfono</p><br><a href="tel:+34678567876"><img src="assets/img/telefono.png" alt="" srcset=""></a><br><br>
-            <p>Correo</p><br><a href="mailto:hgfjhfgjh"><img src="assets/img/email.png" alt="" srcset=""></a>
-          </center>
-        </div>
-        
-  </div>
+    <div class="columns is-mobile">
+      <div class="column">
+        <p class="modal-icon whatsapp">WhatsApp</p>
+        <p class="modal-icon telefono">Teléfono</p>
+        <p class="modal-icon correo">Correo</p>
+      </div>
+      <div class="column">
+        <a href="https://wa.me/523222912787">(322) 2912 787</a>
+        <a href="tel:+34678567876">(346) 7856 7876</a>
+        <a href="mailto:contacto.dra.adriana@gmail.com">contacto.dra.adriana@gmail.com</a>
+      </div>
+    </div>
+    
     <button class="modal-close is-large" aria-label="close"></button>
+  </div>
 </div>
